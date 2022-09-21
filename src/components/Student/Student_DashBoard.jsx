bbcda1127132ecfbbc939cd2679cb8ac3e0155ee
Charlie0560 <chaitanyalokhande87@gmail.com>
2022-09-21 20:39:26 +0530

    Initial Commit

---
+++ src/components/Student/Student_DashBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Student_DashBoard() {
+  return (
+    <div>Student_DashBoard</div>
+  )
+}
+
+export default Student_DashBoard
---
+++ src/components/Teachers/Teachers_Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Teachers_Dashboard() {
+  return (
+    <div>Teachers_Dashboard</div>
+  )
+}
+
+export default Teachers_Dashboard
